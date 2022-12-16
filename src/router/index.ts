db3fe6f0a598da55e60fdd08e58dc84d9d7c323f
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-12-16 09:57:00 +0100

    build issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import DocumentView from "@/views/DocumentView.vue"
 import AboutView from "../views/AboutView.vue";
 import HomeView from "@/views/HomeView.vue"
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
